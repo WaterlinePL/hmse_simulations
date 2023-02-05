f931848b694068cda244bc32b828335202bb1ee2
Observer46 <microplayerpl@gmail.com>
2023-02-05 17:24:12 +0100

    empty new interfaces

---
+++ simulation/simulation_configurator.py
@@ -0,0 +1,15 @@
+from .simulation import Simulation
+from .simulation_chapter import SimulationChapter
+from ..hmse_projects.project_metadata import ProjectMetadata
+
+
+def configure_simulation(project_metadata: ProjectMetadata) -> Simulation:
+    pass
+
+
+def __chapters_from_metadata(project_metadata: ProjectMetadata):
+    pass
+
+
+def __configure_chapter(project_metadata: ProjectMetadata) -> SimulationChapter:
+    pass
